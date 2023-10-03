1f52d91fc805466e07c052b64e6adbbc0517e332
Johnny Nguyen <cuongdn2001@gmail.com>
2023-10-03 11:57:21 +1100

    Remove default persistence controller

--- MyAnime/ContentView.swift
+++ MyAnime/ContentView.swift
@@ -9,6 +9,21 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    
+    init() {
+        let repo = DefaultAnimeFetchRepository()
+        repo.fetchTop(page: 2, subtype: .bydefault) {  (result) in
+            switch result {
+            case .success(let animes):
+                print("Success")
+                print(animes)
+                
+            case .failure(let error):
+                print("Error \(error)")
+            }
+            
+        }
+    }
 
     var body: some View {
        Text("s")
@@ -18,6 +33,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
