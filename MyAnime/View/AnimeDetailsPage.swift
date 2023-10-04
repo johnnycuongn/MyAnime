b7ec9cbad223e49f74f33d174813ffef5c26b346
Johnny Nguyen <cuongdn2001@gmail.com>
2023-10-04 16:02:21 +1100

    Update UI AnimeDetails

--- MyAnime/View/AnimeDetailsPage.swift
+++ MyAnime/View/AnimeDetailsPage.swift
@@ -18,58 +18,98 @@ struct AnimeDetailsPage: View {
 
     var body: some View {
         ZStack {
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 20) {
-                        // Anime Image
-                        if let urlString = anime.images?.jpg.image_url,
-                           let url = URL(string: urlString) {
-                            AsyncImage(url: url)
-                                .frame(maxWidth: .infinity)
-                                .scaledToFit()
-                        } else {
-                            // Fallback if no image_url or if it's invalid
-                            Image(systemName: "photo.fill")
-                                .resizable()
-                                .scaledToFit()
-                        }
-                        
-                        // Anime Title
-                        Text(anime.title)
-                            .font(.title)
-                            .fontWeight(.bold)
-                        
-                        // Anime Description
-                        Text(anime.synopsis ?? "No synopsis available.")
-                            .font(.body)
+            // Background color
+            Color.gray.opacity(0.1)
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    // Anime Image
+                    if let urlString = anime.images?.jpg.image_url,
+                       let url = URL(string: urlString) {
+                        AsyncImage(url: url)
+                            .frame(maxWidth: .infinity)
+//                            .scaledToFit()
+                    } else {
+                        Image(systemName: "photo.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: .infinity)
                     }
-                    .frame(width: .infinity)
-                    .padding(.init(top: 0, leading: 10, bottom: 100, trailing: 10)) // Add padding to avoid overlap with the button
+                    
+                    // Anime Title
+                    Text(anime.title)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.vertical)
+                    
+                    VStack(alignment: .leading, spacing: 2) {
+                        HStack {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.yellow)
+                            Text("\(anime.score ?? 0.0, specifier: "%.1f")")
+                                .font(.caption)
+                            Text("-")
+                            Text("\(anime.scoredBy ?? 0)")
+                                .font(.caption)
+                        }
+                        HStack {
+                            Image(systemName: "person.fill")
+                                .foregroundColor(.blue)
+                            Text("\(anime.members ?? 0)")
+                                .font(.caption)
+                        }
+                        HStack {
+                            Image(systemName: "flame.fill")
+                                .foregroundColor(.red)
+                            Text("\(anime.popularity ?? 0)")
+                                .font(.caption)
+                        }
+                        HStack {
+                            Image(systemName: "film.fill") // or another equivalent icon of your choice
+                                .foregroundColor(.purple)
+                            Text("\(anime.episodes ?? 0)")
+                                .font(.caption)
+                        }
+                    }
+                    
+                    // Anime Description
+                    Text(anime.synopsis ?? "No synopsis available.")
+                        .font(.body)
+                        .padding(.bottom)
                 }
                 .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(8)
+                .shadow(radius: 5)
+                .padding(.bottom, 100) // Extra padding for the floating button
+            }
+            .padding(.horizontal)
             
-                // Favorite Button
-                VStack {
-                    Spacer()
-                    Button(action: {
-                        viewModel.favoriteAnime(id: self.anime.malID, title: self.anime.title, imageURL: self.anime.images?.jpg.image_url ?? "")
-                    }) {
-                        HStack {
-                            Text(viewModel.isAnimeFavorited ? "Favorited" : "Add to Favorites")
-                            Image(systemName: viewModel.isAnimeFavorited ? "heart.fill" : "heart")
-                        }
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(8)
+            // Favorite Button
+            VStack {
+                Spacer()
+                Button(action: {
+                    viewModel.favoriteAnime(id: self.anime.malID, title: self.anime.title, imageURL: self.anime.images?.jpg.image_url ?? "")
+                }) {
+                    HStack {
+                        Text(viewModel.isAnimeFavorited ? "Favorited" : "Add to Favorites")
+                        Image(systemName: viewModel.isAnimeFavorited ? "heart.fill" : "heart")
                     }
                     .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .shadow(radius: 10)
                 }
-            }
-            .frame(maxWidth: .infinity)
-            .onAppear {
-                viewModel.load(anime: anime)
+                .padding(.horizontal)
             }
         }
+        .onAppear {
+            viewModel.load(anime: anime)
+        }
+    }
 }
 struct AnimeDetailsPage_Previews: PreviewProvider {
     static var previews: some View {
